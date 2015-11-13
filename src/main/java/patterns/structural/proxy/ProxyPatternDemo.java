53a60fb681dcf66e853ad5c6d1cb730abc9f7c28
archeeby <a.fenko@gmail.com>
2015-11-14 01:03:40 +0300

    proxy  pattern

---
+++ src/main/java/patterns/structural/proxy/ProxyPatternDemo.java
@@ -0,0 +1,18 @@
+package patterns.structural.proxy;
+
+/* use the ProxyImage to get object of RealImage class when required */
+
+
+public class ProxyPatternDemo {
+
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        //image will be loaded from disk
+        image.display();
+        System.out.println("");
+
+        //image will not be loaded from disk
+        image.display();
+    }
+}
